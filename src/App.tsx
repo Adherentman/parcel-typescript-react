eb4eef3a6b691bdd1d01ad64ec9dec5c25aab814
macxu <adherentxu@gmail.com>
2017-12-20 22:08:24 +0800

    app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+class AppStateType{
+
+};
+
+class AppPropsType{
+
+};
+
+export default class App extends React.Component<AppPropsType,AppStateType>{
+  render(){
+    return <>
+      Hello World!
+      Welcome my Github!
+    </>;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-class App extends React.Component{
-  render(){
-    return <div>Hello World!</div>;
-  }
-}
+import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById("root"));
